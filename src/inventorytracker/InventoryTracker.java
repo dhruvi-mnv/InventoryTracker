563a4f492160457ed8da34076e86ff67d2ce2567
dhruvi-mnv <jariwadh@sheridancollege.ca>
2026-02-19 16:55:22 -0500

    Project setup by Dhruvi

---
+++ src/inventorytracker/InventoryTracker.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package inventorytracker;
+
+/**
+ *
+ * @author dhruv
+ */
+public class InventoryTracker {
+
+    /**
+     * This is an initial version of InventoryTracker.java
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
